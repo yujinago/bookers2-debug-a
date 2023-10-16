f7b342079efc0245b2651f15fc519d842cb9faec
yujinago <138287741+yujinago@users.noreply.github.com>
2023-10-16 08:18:00 +0000

    fix-books-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,13 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    # @books = Book.all
     @book = Book.new
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).sort_by { |x|
+      x.favorited_users.includes(:favorites).where(created_at: from...to).size
+    }.reverse
   end
 
   def create
