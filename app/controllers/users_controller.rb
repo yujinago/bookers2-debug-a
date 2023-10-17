3f1ecfa1098839c877c29f1e1a3f13ad31f62fea
yujinago <138287741+yujinago@users.noreply.github.com>
2023-10-17 03:34:24 +0000

    DM機能-非同期化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
         @room = Room.new
         @entry = Entry.new
       end
-      
+    end
   end
 
   def index
@@ -52,4 +52,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+  
 end
