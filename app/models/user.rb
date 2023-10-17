4516da160ec807da42f9564f1b84d39c92316a1b
yujinago <138287741+yujinago@users.noreply.github.com>
2023-10-17 02:40:22 +0000

    DM機能-非同期前

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
   
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :rooms, through: :entries
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
